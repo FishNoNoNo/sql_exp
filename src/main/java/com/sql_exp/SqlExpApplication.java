ec79f72de32527198f570cbdb8d48ddec1159741
FishNoNoNO <867663065@qq.com>
2025-05-18 12:18:10 +0800

    5.18

---
+++ src/main/java/com/sql_exp/SqlExpApplication.java
@@ -0,0 +1,13 @@
+package com.sql_exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlExpApplication.class, args);
+    }
+
+}
